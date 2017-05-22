902b03c9dd849b3d80fcb2fbec43dfeeecc85fa7
issel parra <isselparra@gmail.com>
2017-05-22 18:40:53 +0000

    Week V MVC : delete + glyphicon

--- app/controllers/curiosities_controller.rb
+++ app/controllers/curiosities_controller.rb
@@ -2,4 +2,11 @@ class CuriositiesController< ApplicationController
     def show
         @curiosity = Curiosity.find(params[:id])
     end
+    
+    def destroy
+        @curiosity = Curiosity.find(params[:id])        # recupere l'identifiant de la curiosite a supprimer
+        @curiosity.delete                               # supprime la curiosite dans la base de donnees
+        
+        redirect_to root_path                           # redirige l'utilisateur vers la vue index
+    end
 end
