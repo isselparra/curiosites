ed307328af433345c70583c74daeeb0d653b9330
issel parra <isselparra@gmail.com>
2017-05-29 18:03:58 +0000

    Week VI : Comprendre le V et le C de MVC (2/2)

--- app/controllers/curiosities_controller.rb
+++ app/controllers/curiosities_controller.rb
@@ -9,4 +9,29 @@ class CuriositiesController< ApplicationController
         
         redirect_to root_path                           # redirige l'utilisateur vers la vue index
     end
+    
+    def new
+        @curiosity = Curiosity.new                      # crée une curiosité 'vide'
+    end
+    
+    def create
+        @curiosity = Curiosity.new(curiosity_params)
+        # instance une nouvelle curiosité
+        # avec les parametres contenus dans le formulaire
+        # et passés à la variable params
+        
+        if @curiosity.save # si la curiosité est sauvegardée sans erreurs
+            redirect_to @curiosity # alors on l'affiche
+        else
+            render 'new' # sinon on reste sur la page de création
+        end
+    end
+    
+    private
+    
+    # définit les parametres acceptés par le formulaire
+    # permet d'éviter à un utilisateur malveillant de passar d'autres parametres
+    def curiosity_params
+        params.require(:curiosity).permit(:name, :description, :image_url, :image_text, :category)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   
+  post 'curiosities', to: 'curiosities#create', as: 'curiosities'
+  get 'curiosities/new', to: 'curiosities#new', as: 'new_curiosity'
   get 'curiosities/:id', to: 'curiosities#show', as: 'curiosity'
   delete 'curiosities/:id', to: 'curiosities#destroy'
 
